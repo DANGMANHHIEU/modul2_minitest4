ce6b24370d037316d175e7edbdd4b4b5158101a6
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-13 14:57:04 +0700

    tạo class

---
+++ src/minitest/Iphone.java
@@ -0,0 +1,8 @@
+package minitest;
+
+public interface Iphone {
+    void searchPhone(String name);
+    void sort();
+    void  display(Type type);
+
+}
---
+++ src/minitest/PhoneBookManage.java
@@ -0,0 +1,34 @@
+package minitest;
+
+public class PhoneBookManage extends Phone implements Iphone{
+
+    @Override
+    public void searchPhone(String name) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void display(Type type) {
+
+    }
+
+    @Override
+    void insertPhone(Contact contact) {
+
+    }
+
+    @Override
+    void removePhone(String name) {
+
+    }
+
+    @Override
+    void updatePhone(String name, String newPhone) {
+
+    }
+}
