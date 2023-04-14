49175783f98c387eb508935220aae804044adb55
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-14 17:01:45 +0700

    thuộc tính danh bạ

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -4,12 +4,17 @@ import model.Type;
 
 import java.io.Serializable;
 
-public class Contact implements Serializable {
+public class Contact implements Serializable  {
     private String name;
     private String phoneNumber;
     private Type type;
 
     public Contact() {
+    }
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
     }
 
     public Contact(String name, String phoneNumber, Type type) {
@@ -25,6 +30,17 @@ public class Contact implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    //lấy tên để sắp xếp
+//    public String get_name(){
+//        String n_ame= this.name .trim();//cắt khoảng trắng 2 đầu
+//        if(n_ame.indexOf(" ")>=0){
+//            int index= n_ame.lastIndexOf(" ");
+//            return n_ame.substring(index+1);
+//        }else {
+//            return n_ame;
+//        }
+//    }
 
     public String getPhoneNumber() {
         return phoneNumber;
@@ -50,4 +66,6 @@ public class Contact implements Serializable {
                 ", type= " + type +
                 "}";
     }
+
+
 }
