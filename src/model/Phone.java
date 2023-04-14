2cef7a5f5b73dc5091c6279f84fffcbef83eb6af
DANGMANHHIEU <manhhieu261296@gmail.com>
2023-04-14 16:57:54 +0700

    abstract Phone

--- src/model/Phone.java
+++ src/model/Phone.java
@@ -2,7 +2,7 @@ package model;
 
 public abstract class Phone {
     protected abstract void display(Type type);
-    protected abstract void insertPhone(Contact contact);
+    protected abstract void insertPhone(Contact contact1);
    protected abstract void removePhone(String name);
   protected abstract void updatePhone(String name, String newPhone);
 
